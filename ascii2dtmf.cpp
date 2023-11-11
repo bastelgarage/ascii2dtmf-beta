7c7398f7e692e83a6acc0a728f512181d3ca6cb2
Alf Müller <31915686+AlfMueller@users.noreply.github.com>
2023-11-11 15:33:41 +0100

    Update ascii2dtmf.cpp

--- ascii2dtmf.cpp
+++ ascii2dtmf.cpp
@@ -11,17 +11,22 @@ void ascii2dtmf::encodeToDTMF(char asciiChar, char &tone1, char &tone2) {
 }
 
 char ascii2dtmf::decodeFromDTMF(char tone1, char tone2) {
-    int tone1Index = -1;
-    int tone2Index = -1;
+    int tone1Index = findToneIndex(tone1);
+    int tone2Index = findToneIndex(tone2);
 
-    // Finde die Indizes der Töne
-    for (int i = 0; i < 16; i++) {
-        if (dtmfTones[i] == tone1) tone1Index = i;
-        if (dtmfTones[i] == tone2) tone2Index = i;
+    if (tone1Index == -1 || tone2Index == -1) {
+        // Fehlerhafte Tonangaben
+        return '\0'; // Rückgabe eines Null-Zeichens bei Fehlern
     }
-
-    if (tone1Index == -1 || tone2Index == -1) return '\0'; // Fehlerfall
 
     return (char)(tone1Index * 16 + tone2Index);
 }
 
+int ascii2dtmf::findToneIndex(char tone) {
+    for (int i = 0; i < 16; i++) {
+        if (dtmfTones[i] == tone) {
+            return i;
+        }
+    }
+    return -1; // Ton not found
+}
